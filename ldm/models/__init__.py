fead651de83305af040c7fe5863d9b714b69bd33
heimaoqqq <heimaoqqq@gmail.com>
2025-06-16 17:06:36 +0800

    修复：更新模型导入，添加混合注意力到导出列表

--- ldm/models/__init__.py
+++ ldm/models/__init__.py
@@ -2,5 +2,6 @@
 LDM 模型包
 """
 from .unet import create_unet_model
+from .mixed_attention_unet import create_mixed_attention_unet
  
-__all__ = ["create_unet_model"] 
+__all__ = ["create_unet_model", "create_mixed_attention_unet"] 
